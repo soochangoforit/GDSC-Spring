6419acc8eea3773938bcd8add1d2958d766e2c4e
soochangoforit <tncksdl05@gmail.com>
2022-04-02 15:08:03 +0900

    setting

---
+++ src/main/java/com/jojoldu/book/GDSCSpringBoot2/GdscSpringBoot2Application.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.GDSCSpringBoot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdscSpringBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdscSpringBoot2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jojoldu/book/GDSCSpringBoot2/GdscSpringBoot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.GDSCSpringBoot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdscSpringBoot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
