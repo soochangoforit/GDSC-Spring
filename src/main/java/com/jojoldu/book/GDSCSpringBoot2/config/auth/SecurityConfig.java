741f55ec1061dd14022c6b9e12cc84649d65cf97
soochangoforit <tncksdl05@gmail.com>
2022-05-13 18:58:34 +0900

    5장 Oauth 구현

---
+++ src/main/java/com/jojoldu/book/GDSCSpringBoot2/config/auth/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.jojoldu.book.GDSCSpringBoot2.config.auth;
+
+import com.jojoldu.book.GDSCSpringBoot2.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name()) //게시글 등록 과정에 /api 경로로 들어간다. USER가 아니면 못 한다.
+                    .anyRequest().authenticated() // 나머지 예를 들어 로그인 하지 않고 /post/save 할 경우 , 인증이 되어 있지 않기 때문에 로그인하라고 창이 나온다 Oauth로
+                .and()
+                    .formLogin()
+                    .loginPage("/test3")
+                     .permitAll()
+                .and()
+                .logout()
+                    .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                        .userService(customOAuth2UserService);
+    }
+
+
+}
---
+++ src/main/java/com/jojoldu/book/GDSCSpringBoot2/config/auth/dto/SessionUser.java
@@ -0,0 +1,26 @@
+package com.jojoldu.book.GDSCSpringBoot2.config.auth.dto;
+
+
+import com.jojoldu.book.GDSCSpringBoot2.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+//session에 user라는 이름으로 SessionUser 클래스 dto 객체르 저장
+// sessionUser에는 인증된 사용자 정보만 필요
+// 즉 세션 객체는 이미 DB에 저장된 ( 즉, 인증된 사용자 ) 만을 저장하고 있다.
+// 무조건 session에 들어가기전에 Db에 해당 유저 정보가 들어가 있다.
+@Getter
+public class SessionUser implements Serializable {
+
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+
+}
