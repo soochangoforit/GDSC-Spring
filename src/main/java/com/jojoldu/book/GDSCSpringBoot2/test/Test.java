512958b2a44fd876d2a5e9ae017c6837f8d4e19f
soochangoforit <tncksdl05@gmail.com>
2022-05-21 06:22:05 +0900

    Test weq

--- src/main/java/com/jojoldu/book/GDSCSpringBoot2/test/Test.java
+++
@@ -1,6 +0,0 @@
-package com.jojoldu.book.GDSCSpringBoot2.test;
-
-public class Test {
-
-    //github에서 bracch 만들고 test
-}
--- src/main/java/com/jojoldu/book/GDSCSpringBoot2/test/TestForThird.java
+++
@@ -1,8 +0,0 @@
-package com.jojoldu.book.GDSCSpringBoot2.test;
-
-public class TestForThird {
-
-
-
-
-}
